5f556954a08dc9d71563db56009852581b7f6058
happyornothappy <101691696+happyornothappy@users.noreply.github.com>
2023-11-07 21:21:15 +0900

    헤더 영역 데이터 작업

---
+++ src/components/header/Sns.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { snsLink } from '../../data/header'
+import { useLocation } from 'react-router-dom'
+
+const Sns = () => {
+    const location=useLocation();
+  return (
+    <div className='header__sns'>
+                <ul>
+                    {snsLink.map((sns, key) => (
+                        <li key={key} className={location.pathname === sns.src ? 'active' : ''}>
+                            <a href={sns.url} 
+                            target='_blank' 
+                            rel='noopener noreferrer' 
+                            aria-label={sns.title}>
+
+                                <span>{sns.icon}</span>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+  )
+}
+
+export default Sns
